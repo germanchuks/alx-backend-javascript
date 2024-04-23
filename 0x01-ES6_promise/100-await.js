cb9d76e6d2e2a9e8d38280ac3fbc3b53eeb4822d
Daniel German <danielchuksgerman@gmail.com>
2024-04-23 14:17:16 +0000

    Task 10

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+    try {
+        return {
+            photo: await uploadPhoto(),
+            user: await createUser()
+        };
+    } catch (error) {
+        return {
+            photo: null,
+            user: null,
+        };
+    }
+}
+
+export default asyncUploadUser;
