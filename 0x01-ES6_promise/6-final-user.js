f30dc30d595e62d5d45caae15ddb62914301182c
Daniel German <danielchuksgerman@gmail.com>
2024-04-23 13:51:05 +0000

    Task 5

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+        .then(res => {
+            return res.map(res => ({
+                status: res.status,
+                value: res.status === 'fulfiled' ? res.value : res.reason
+            }));
+        });
+}
