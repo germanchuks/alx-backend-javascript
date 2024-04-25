0adbfd2de123453df5ff2efb234cc08fd00933ad
Daniel German <danielchuksgerman@gmail.com>
2024-04-25 14:13:48 +0000

    Task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+// 6-sky_high.js
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Getter for floors attribute
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
