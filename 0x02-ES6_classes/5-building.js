8a899650776270f8b0536b15da30da865c199aa2
Daniel German <danielchuksgerman@gmail.com>
2024-04-25 14:01:21 +0000

    Task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+// 5-building.js
+
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  // Getter for sqft attribute
+  get sqft() {
+    return this._sqft;
+  }
+}
