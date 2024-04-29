380151a5b98ff0b0357393abd5a9914752ada3bc
Daniel German <danielchuksgerman@gmail.com>
2024-04-29 12:31:19 +0000

    Task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+// 5-typed_arrays.js
+
+export default function createInt8TypedArray(length, position, value) {
+    if (position < 0 || position >= length) {
+        throw new Error('Position outside range');
+    }
+
+    const buffer = new ArrayBuffer(length);
+    const dv = new DataView(buffer);
+    dv.setInt8(position, value);
+
+    return dv;
+}
