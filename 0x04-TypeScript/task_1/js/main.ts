572162928828f442ea6aff4e15e28d4946f0dcd3
Daniel German <danielchuksgerman@gmail.com>
2024-04-30 14:41:18 +0000

    Student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,5 +24,36 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`
 }
 
-// console.log(printTeacher("John", "Doe"));
 
+// Interface for the StudentClass constructor parameters
+interface StudentConstructor {
+  firstName: string, lastName: string;
+}
+
+// Interface for the StudentClass
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class Student implements StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor({firstName, lastName}: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+// const student1 = new Student({ firstName: "Daniel", lastName: "Chuks" });
+// console.log(student1.workOnHomework())
+// console.log(student1.displayName())
