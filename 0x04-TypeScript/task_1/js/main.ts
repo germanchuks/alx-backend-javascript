9d0bf661b2170988fa845e55eee640e1ade3778e
Daniel German <danielchuksgerman@gmail.com>
2024-04-30 14:20:34 +0000

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,16 +10,19 @@ interface Teacher {
     [key: string]: any;
 }
 
+// Interface for Directors
 interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-// const director1: Directors = {
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   location: 'London',
-//   fullTimeEmployee: true,
-//   numberOfReports: 17,
-// };
-// console.log(director1);
+// Interface for printTeacher function
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`
+}
+
+// console.log(printTeacher("John", "Doe"));
 
