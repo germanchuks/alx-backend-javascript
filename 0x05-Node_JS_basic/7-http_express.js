60eece09b3db0ab68605f5f5744c40c2a1dc62db
Daniel German <danielchuksgerman@gmail.com>
2024-06-14 16:45:31 +0100

    Create HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+// Creating a HTTP server using Express
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+const PORT = 1245;
+const filePath = process.argv[2];
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+    res.write('This is the list of our students\n')
+    countStudents(filePath)
+        .then((result) => {
+            res.write(result);
+            res.end();
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+})
+
+app.listen(PORT);
+
+module.exports = app;
