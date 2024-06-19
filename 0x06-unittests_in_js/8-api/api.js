c8daaabad4ab7377f6972cee4e78478563b531c9
Daniel German <danielchuksgerman@gmail.com>
2024-06-19 21:21:56 +0100

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = 7865;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
 app.listen(PORT, () => {
-    console.log(`API available on localhost port ${PORT}`);
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
