8c455994d8c686a13a52b7e57e07f98cf64f2d34
Daniel German <danielchuksgerman@gmail.com>
2024-06-19 20:36:47 +0100

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('resolves with success data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        expect(data).toEqual({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
