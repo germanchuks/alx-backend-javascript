5faa300defa36aac919ec89406aac833cf6ad4f9
Daniel German <danielchuksgerman@gmail.com>
2024-06-19 19:47:28 +0100

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import calculateNumber from './2-calcul_chai.js';
+
+describe('calculateNumber', function () {
+  describe('SUM', function () {
+    it('should return the sum of rounded numbers', function () {
+      expect(calculateNumber('SUM', 2.3, 2.5)).to.equal(5);
+      expect(calculateNumber('SUM', 3.4, 5.4)).to.equal(8);
+      expect(calculateNumber('SUM', -2.2, -4.5)).to.equal(-6);
+    });
+  });
+
+  describe('SUBTRACT', function () {
+    it('should return the difference of rounded numbers', function () {
+      expect(calculateNumber('SUBTRACT', 2.3, 2.5)).to.equal(-1);
+      expect(calculateNumber('SUBTRACT', 3.4, 5.4)).to.equal(-2);
+      expect(calculateNumber('SUBTRACT', -2.2, -4.5)).to.equal(2);
+    });
+  });
+
+  describe('DIVIDE', function () {
+    it('should return the division of rounded numbers', function () {
+      expect(calculateNumber('DIVIDE', 1.3, 3.5)).to.equal(0.25);
+      expect(calculateNumber('DIVIDE', 3.4, 5.4)).to.equal(0.6);
+    });
+
+    it('should return Error when dividing by 0', function () {
+      expect(calculateNumber('DIVIDE', 2.3, 0)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 3.4, 0.2)).to.equal('Error');
+    });
+  });
+});
